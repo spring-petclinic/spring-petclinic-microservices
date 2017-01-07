31cd26286c5efaf07ab0e2cc665931e501798c8d
Antoine Rey <antoine.rey@gmail.com>
2017-01-07 19:18:50 +0100

    Initialize Spring Boot admin

---
+++ spring-petclinic-admin/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -0,0 +1,17 @@
+package org.springframework.samples.petclinic.admin;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@EnableDiscoveryClient
+public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
+}
