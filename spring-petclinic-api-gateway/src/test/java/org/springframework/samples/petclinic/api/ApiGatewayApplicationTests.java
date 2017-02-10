deebc6ccab973144b458adff74275f9f140a4d41
Antoine Rey <antoine.rey@gmail.com>
2017-02-10 19:42:39 +0100

    Fix #40 Remove error logs from ApiGatewayApplicationTests (#45)

--- spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/ApiGatewayApplicationTests.java
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/ApiGatewayApplicationTests.java
@@ -3,9 +3,11 @@ package org.springframework.samples.petclinic.api;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@ActiveProfiles("test")
 @SpringBootTest
 public class ApiGatewayApplicationTests {
 
