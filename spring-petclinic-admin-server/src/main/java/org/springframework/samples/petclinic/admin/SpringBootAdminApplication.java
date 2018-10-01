3dd574e457eaba56b1b2e79502bebf7491d6a922
Antoine Rey <antoine.rey@gmail.com>
2018-10-01 08:28:14 +0200

    Replace @Configuration and @EnableAutoConfiguration by @SpringBootApplication (#99)

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -17,12 +17,10 @@ package org.springframework.samples.petclinic.admin;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
