98fba5447775099f28762061782577afa3a9c545
Antoine Rey <antoine.rey@gmail.com>
2019-04-03 08:17:17 +0200

    Migrate from Netflix Zuul to Spring Cloud Gateway #117

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
@@ -15,19 +15,26 @@
  */
 package org.springframework.samples.petclinic.api;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
 
 /**
  * @author Maciej Szarlinski
  */
-@EnableZuulProxy
 @EnableDiscoveryClient
 @EnableCircuitBreaker
 @SpringBootApplication
@@ -41,5 +48,20 @@ public class ApiGatewayApplication {
     @LoadBalanced
     RestTemplate loadBalancedRestTemplate() {
         return new RestTemplate();
+    }
+
+    @Value("classpath:/static/index.html")
+    private Resource indexHtml;
+
+    /**
+     * workaround solution for forwarding to index.html
+     * @see <a href="https://github.com/spring-projects/spring-boot/issues/9785">#9785</a>
+     */
+    @Bean
+    RouterFunction<?> routerFunction() {
+        RouterFunction router = RouterFunctions.resources("/**", new ClassPathResource("static/"))
+            .andRoute(RequestPredicates.GET("/"),
+                request -> ServerResponse.ok().contentType(MediaType.TEXT_HTML).syncBody(indexHtml));
+        return router;
     }
 }
