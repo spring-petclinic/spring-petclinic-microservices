ee8fc02e07ad7ef2aafda66b2fe0486e5e614601
Antoine Rey <antoine.rey@gmail.com>
2019-11-20 20:23:19 +0100

    Remove the @EnableCircuitBreaker annotation and Hystrix dependencies #117

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
@@ -23,7 +23,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.ReactiveResilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -44,7 +43,6 @@ import java.time.Duration;
  * @author Maciej Szarlinski
  */
 @EnableDiscoveryClient
-@EnableCircuitBreaker
 @SpringBootApplication
 public class ApiGatewayApplication {
 
