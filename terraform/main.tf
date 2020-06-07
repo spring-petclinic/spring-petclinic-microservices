8f50964d39e797354de86fa26a7b025917537382
yucwan <49381248+yucwan@users.noreply.github.com>
2020-06-07 21:46:46 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,33 +14,7 @@ variable "spring_cloud_service" {
 variable "api_gateway" {
     type = string
 }
-variable "admin_server" {
-    type = string
-}
-variable "customers_service" {
-    type = string
-}
-variable "visits_service" {
-    type = string
-}
-variable "vets_service" {
-    type = string
-}
-variable "mysql_server_name" {
-    type = string
-}
-variable "mysql_server_admin_name" {
-    type = string
-}
-variable "mysql_server_admin_password" {
-    type = string
-}
-variable "mysql_database_name" {
-    type = string
-}
-variable "dev_machine_ip" {
-    type = string
-}
+
 
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group
