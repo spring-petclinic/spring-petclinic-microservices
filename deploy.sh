08ef557797cde8bede0031f89e103d03f3330aba
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-11-26 23:59:05 +0100

    update_shell script

--- deploy.sh
+++ deploy.sh
@@ -1,47 +1,14 @@
 #!/bin/bash
 
-echo "====================================="
-echo "====  Package Admin Server  ========="
-echo "====================================="
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-admin-server/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-api-gateway/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-config-server/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-customers-service/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-discovery-server/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-vets-service/pom.xml
+./mvnw mvn clean install spring-boot:build-image -pl ./spring-petclinic-visits-service/pom.xml
 
-./mvnw clean package -pl ./spring-petclinic-admin-server/pom.xml
 
-
-echo "====================================="
-echo "====  Package Api Gateway   ========="
-echo "====================================="
-
-./mvnw clean package -pl ./spring-petclinic-api-gateway/pom.xml
-
-echo "======================================="
-echo "====  Package Config Server   ========="
-echo "======================================="
-
-./mvnw clean package -pl ./spring-petclinic-config-server/pom.xml
-
-echo "==========================================="
-echo "====  Package customers service   ========="
-echo "==========================================="
-
-./mvnw clean package -pl ./spring-petclinic-customers-service/pom.xml
-
-echo "==========================================="
-echo "====  Package discovery server    ========="
-echo "==========================================="
-
-./mvnw clean package -pl ./spring-petclinic-discovery-server/pom.xml
-
-echo "======================================="
-echo "====  Package Vets Service    ========="
-echo "======================================="
-
-./mvnw clean package -pl ./spring-petclinic-vets-service/pom.xml
-
-echo "========================================="
-echo "====  Package Visits Service    ========="
-echo "========================================="
-
-./mvnw clean package -pl ./spring-petclinic-visits-service/pom.xml
 
 
 
