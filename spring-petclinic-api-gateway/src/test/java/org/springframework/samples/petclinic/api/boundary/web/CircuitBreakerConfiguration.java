69f5749aee32bcd62ae6369597fe3d4b81226593
Antoine Rey <antoine.rey@gmail.com>
2021-07-04 19:40:39 +0200

    Upgrade to Spring Cloud 2020.0.2 and Spring Boot 2.5.1

---
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/boundary/web/CircuitBreakerConfiguration.java
@@ -0,0 +1,20 @@
+package org.springframework.samples.petclinic.api.boundary.web;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CircuitBreakerConfiguration {
+
+    @Bean
+    public CircuitBreakerRegistry circuitBreakerRegistry() {
+        return CircuitBreakerRegistry.ofDefaults();
+    }
+
+    @Bean
+    public TimeLimiterRegistry timeLimiterRegistry() {
+        return TimeLimiterRegistry.ofDefaults();
+    }
+}
