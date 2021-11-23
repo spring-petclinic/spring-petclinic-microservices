e122b54d25308f8041a92ace4e1ea0c675ef1c11
Anton Patsev <patsev.anton@gmail.com>
2021-11-23 19:16:23 +0600

    docker image tag adenmin latest

--- mcr-pet-images-push.sh
+++ mcr-pet-images-push.sh
@@ -4,12 +4,12 @@ ACTUAL_VERSION="0.4.0"
 
 echo "chagne latest tag to $ACTUAL_VERSION"
 
-docker image tag drill4j/spring-petclinic-config-server:latest drill4j/spring-petclinic-config-server:$ACTUAL_VERSION
-docker image tag drill4j/spring-petclinic-discovery-server:latest drill4j/spring-petclinic-discovery-server:$ACTUAL_VERSION
-docker image tag drill4j/spring-petclinic-customers-service:latest drill4j/spring-petclinic-customers-service:$ACTUAL_VERSION
-docker image tag drill4j/spring-petclinic-visits-service:latest drill4j/spring-petclinic-visits-service:$ACTUAL_VERSION
-docker image tag drill4j/spring-petclinic-vets-service:latest drill4j/spring-petclinic-vets-service:$ACTUAL_VERSION
-docker image tag drill4j/spring-petclinic-api-gateway:latest drill4j/spring-petclinic-api-gateway:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-config-server:latest drill4j/spring-petclinic-config-server:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-discovery-server:latest drill4j/spring-petclinic-discovery-server:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-customers-service:latest drill4j/spring-petclinic-customers-service:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-visits-service:latest drill4j/spring-petclinic-visits-service:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-vets-service:latest drill4j/spring-petclinic-vets-service:$ACTUAL_VERSION
+docker image tag adenmin/spring-petclinic-api-gateway:latest drill4j/spring-petclinic-api-gateway:$ACTUAL_VERSION
 
 echo "push images"
 
