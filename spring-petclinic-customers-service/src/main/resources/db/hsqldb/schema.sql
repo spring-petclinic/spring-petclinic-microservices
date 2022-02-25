08636e95c2cdab7074099f72bfaf3c36c957a6e7
Ramazan Sakin <ramazansakin63@gmail.com>
2022-02-25 13:37:40 +0300

    fix: telephone input field pattern & size conditions set & updated on db script

--- spring-petclinic-customers-service/src/main/resources/db/hsqldb/schema.sql
+++ spring-petclinic-customers-service/src/main/resources/db/hsqldb/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE owners (
   last_name  VARCHAR(30),
   address    VARCHAR(255),
   city       VARCHAR(80),
-  telephone  VARCHAR(20)
+  telephone  VARCHAR(12)
 );
 CREATE INDEX owners_last_name ON owners (last_name);
 
