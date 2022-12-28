5786d53d4b0a5696edd0159aff7cf013cc49dc6d
Antoine Rey <antoine.rey@free.fr>
2022-12-28 10:55:23 +0100

    Upgrade to Spring Cloud 2022.2, Spring Boot 3 and Java 17 #217 (#221)
    * Use Github Actions for CI
    
    * Bump to Spring Cloud 2022.0.0-M1 and Spring Boot 3.0.0-M1
    
    * Bump to Spring Cloud 2022.0.0 and Spring Boot 3.0.1
    
    * Move from javax to jakarta package
    
    * Artefact mysql-connector-java relocated to com.mysql:mysql-connector-j
    
    * Upgrade Java CI to Java 17
    
    * Spring Cloud Sleuth migrated to Micrometer Tracing
    
    * Spring Cloud Sleuth migrated to Micrometer Tracing
    
    * Bump to Spring Boot Admin 3.0.0-M8
    
    * Switch vers cache from ehchace to caffeine
    
    * Dockerfile now use eclipse-temurin:17
    
    * Add MetricConfig because Micrometer’s Spring Boot configuration does not more recognize @Timed on arbitrary methods.
    
    * Eclipse Temurin with Java 17 as Docker base image
    
    * Upgrade Grafana Petclinic dashboard JSON config with RestController method name

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/model/PetType.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/model/PetType.java
@@ -18,12 +18,12 @@ package org.springframework.samples.petclinic.customers.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 /**
  * @author Juergen Hoeller
--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/Visit.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/model/Visit.java
@@ -21,15 +21,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.Size;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.Size;
 import java.util.Date;
 
 /**
