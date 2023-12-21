2f60ee38733f4f08de70ced8d305ccdb0e64571d
Pieter Hagen <phagen@splunk.com>
2023-12-21 10:22:05 +0000

    updates to push script

--- scripts/push_containers.sh
+++ scripts/push_containers.sh
@@ -20,6 +20,17 @@ fi
 # Loop through each container and push it to the registry
 for container in "${containers[@]}"
 do
+    echo "Tagging $container with tag $tag..."
+    
+    # Tag the Docker container
+    docker tag "$container" "$registry/$namespace/$container:$tag"
+
+    # Check if tagging was successful
+    if [ $? -ne 0 ]; then
+        echo "Failed to tag $container with tag $tag. Exiting..."
+        exit 1
+    fi
+
     echo "Pushing $container with tag $tag to $registry/$namespace..."
 
     # Push the Docker container with the specified tag
