77e4e8a12ce8e2617b59108cd445f232c0e4c845
Pieter Hagen <phagen@splunk.com>
2023-12-21 10:16:51 +0000

    push script

---
+++ scripts/push_containers.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Array of Docker container names
+containers=("spring-petclinic-api-gateway" "spring-petclinic-discovery-server" "spring-petclinic-config-server" "spring-petclinic-visits-service" "spring-petclinic-vets-service" "spring-petclinic-customers-service" "spring-petclinic-admin-server")
+
+# Docker registry URL
+registry="quay.io"
+
+# Docker repository namespace
+namespace="phagen"
+
+# Check if a tag was provided as an argument
+if [ $# -eq 0 ]; then
+    echo "No tag provided. Using 'latest' as the default tag."
+    tag="latest"
+else
+    tag="$1"
+fi
+
+# Loop through each container and push it to the registry
+for container in "${containers[@]}"
+do
+    echo "Pushing $container with tag $tag to $registry/$namespace..."
+
+    # Push the Docker container with the specified tag
+    docker push "$registry/$namespace/$container:$tag"
+
+    # Check the push result
+    if [ $? -eq 0 ]; then
+        echo "Successfully pushed $container with tag $tag"
+        echo "Removing local $container image..."
+        docker rmi "$registry/$namespace/$container:$tag"
+    else
+        echo "Failed to push $container with tag $tag"
+        exit 1
+    fi
+done
+
+docker images
