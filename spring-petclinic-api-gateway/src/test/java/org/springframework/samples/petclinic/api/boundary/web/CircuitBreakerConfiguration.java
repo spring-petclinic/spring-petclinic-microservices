412c9bf2348f226bdc33223719d1842a905049ba
Shobha Kamath <shobha.s.kamath@gmail.com>
2023-12-28 15:06:36 +0400

    Upgrade to Spring Cloud 2023.0.0 (#249)
    * lombok issue fix
    
    * Added comment - lombok issue fix
    
    * Upgrade to Spring Cloud 2023.0.0
    
    * Versioning the petclinic microservice
    
    * Versioning the petclinic microservice
    
    * Fix the docker image entrypoint
    
    * reverting
    
    * Fixing the entrypoint class

--- spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/boundary/web/CircuitBreakerConfiguration.java
+++ spring-petclinic-api-gateway/src/test/java/org/springframework/samples/petclinic/api/boundary/web/CircuitBreakerConfiguration.java
@@ -2,8 +2,10 @@ package org.springframework.samples.petclinic.api.boundary.web;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
 import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class CircuitBreakerConfiguration {
@@ -17,4 +19,11 @@ public class CircuitBreakerConfiguration {
     public TimeLimiterRegistry timeLimiterRegistry() {
         return TimeLimiterRegistry.ofDefaults();
     }
+
+    @Bean
+    @Primary
+    public Resilience4JConfigurationProperties resilience4JConfigurationProperties(){
+        return  new Resilience4JConfigurationProperties();
+    }
+
 }
