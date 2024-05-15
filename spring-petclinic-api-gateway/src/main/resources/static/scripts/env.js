58332a6824e36e1923da9eee7652c53efb1990bc
Pieter Hagen <phagen@splunk.com>
2024-05-15 12:10:51 +0200

    removed js error

--- spring-petclinic-api-gateway/src/main/resources/static/scripts/env.js
+++ spring-petclinic-api-gateway/src/main/resources/static/scripts/env.js
@@ -4,8 +4,4 @@ env = {
     RUM_APP_NAME: '',
     RUM_ENVIRONMENT: ''
   }
-  // non critical error so it shows in RUM when the realm is set
-  if (env.RUM_REALM != "") {
-    let showJSErrorObject = false;
-    showJSErrorObject.property = 'true';
-  }
+  
