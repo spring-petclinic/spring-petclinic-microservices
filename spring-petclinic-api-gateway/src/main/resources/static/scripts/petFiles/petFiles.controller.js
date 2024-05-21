12dd13eb4a4ada01b199cbf43950ded8c5e5b7b8
Taylor, Joshua-Butah <joshua-butah.taylor@haw-hamburg.de>
2024-05-21 11:21:22 +0200

    Upload wird für nach dem Timeout (10s) wiederholt

--- spring-petclinic-api-gateway/src/main/resources/static/scripts/petFiles/petFiles.controller.js
+++ spring-petclinic-api-gateway/src/main/resources/static/scripts/petFiles/petFiles.controller.js
@@ -23,16 +23,66 @@ angular.module('petFiles')
             formData.append('date', $filter('date')(self.date, "yyyy-MM-dd"));
             formData.append('description', self.desc);
 
-            $http.post(url, formData, {
-                transformRequest: angular.identity,
-                headers: { 'Content-Type': undefined }
-            }).then(function () {
-                alert("File: " + file.name + " was uploaded successfully!\n" +"file size: "+ file.size/1024/1024 + " MB");
-                return $http.get(url);
-            }).then(function (response) {
-                self.files = response.data;
-                $state.go('ownerDetails', { ownerId: ownerId });
-            });
+            // Setzen Sie die Variable auf true, um den Spinner anzuzeigen
+            //self.uploading = true;
+
+
+            var upload = function() {
+                let  uploadCompleted = false;
+                let repeatUpload = false;
+                $http.post(url, formData, {
+                    transformRequest: angular.identity,
+                    headers: { 'Content-Type': undefined }
+                }).then(function () {
+                    if(!repeatUpload ){
+                        uploadCompleted = true;
+                        console.log("File: " + file.name + " was uploaded successfully!\n" +"file size: "+ file.size/1024/1024 + " MB");
+                        alert("File: " + file.name + " was uploaded successfully!\n" +"file size: "+ file.size/1024/1024 + " MB");
+                        return $http.get(url);
+                    }
+                    //console.log("Upload hat zu lange gedauert 11:13");
+
+                }).then(function (response) {
+                    self.files = response.data;
+                    $state.go('ownerDetails', { ownerId: ownerId });
+                }).catch(function (error) {
+                    //alert("Es gibt einen Fehler beim Hochladen der Datei: " + error.message);
+                });
+
+                setTimeout(function() {
+                    if (uploadCompleted) {
+                        repeatUpload = false;
+                    } else {
+                        repeatUpload = true;
+                        //alert("Upload dauert zu lange, versuche erneut... jbt2");
+                        console.log("Upload dauert zu lange, versuche erneut...11:13");
+                        upload();
+                    }
+                }, 10000);
+            };
+
+            upload();
+
+            // setTimeout(function() {
+            //     if (!uploadCompleted) {
+            //         //alert("Upload dauert zu lange, versuche erneut...");
+            //         upload();
+            //     }
+            // }, 5000);
+
+
+            // $http.post(url, formData, {
+            //     transformRequest: angular.identity,
+            //     headers: { 'Content-Type': undefined }
+            // }).then(function () {
+            //     alert("File: " + file.name + " was uploaded successfully!\n" +"file size: "+ file.size/1024/1024 + " MB");
+            //     return $http.get(url);
+            // }).then(function (response) {
+            //     self.files = response.data;
+            //     $state.go('ownerDetails', { ownerId: ownerId });
+            // }).catch(function (error) {
+            //     alert("Fehler beim Hochladen der Datei: " + error.message);
+            // });
 
         };
 
