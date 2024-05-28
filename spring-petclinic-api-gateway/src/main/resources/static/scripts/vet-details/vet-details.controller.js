a5ed52fff9034dc7c581934609e5da33bc74c9e1
TheNinaNiesche <nina.niesche@gmx.de>
2024-05-28 10:33:15 +0200

    feat(infiniteLoop): added Dropdown to vet-details

--- spring-petclinic-api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
+++ spring-petclinic-api-gateway/src/main/resources/static/scripts/vet-details/vet-details.controller.js
@@ -3,8 +3,15 @@
 angular.module('vetDetails')
     .controller('VetDetailsController', ['$http', '$stateParams', function ($http, $stateParams) {
         var self = this;
+        self.selectedVetId = null;
+
+        $http.get('api/vet/vets').then(function (resp) {
+            self.vetList = resp.data;
+        });
 
         $http.get('api/gateway/vets/' + $stateParams.vetId).then(function (resp) {
             self.vet = resp.data;
         });
+
+
     }]);
