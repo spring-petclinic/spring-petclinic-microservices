14e4f102d9ce7cc644dd7b885b4011ba2b24224d
krisschaaf <kris-schaaf@gmx.de>
2024-06-03 11:34:16 +0200

    add error handling to webapp fe

--- WebApp/make-it-crash-fe/src/pages/scenarios/custom/ScenarioWithParams.tsx
+++ WebApp/make-it-crash-fe/src/pages/scenarios/custom/ScenarioWithParams.tsx
@@ -7,6 +7,7 @@ import { useCollapse } from 'react-collapsed';
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import Autocomplete from '@mui/material/Autocomplete';
+import Alert from '@mui/material/Alert';
 
 function Scenario({ title, text }: { title: string, text: string}) {
     const { getCollapseProps, getToggleProps, isExpanded } = useCollapse();
@@ -16,12 +17,40 @@ function Scenario({ title, text }: { title: string, text: string}) {
     const [showSpinner, setShowSpinner] = useState(false);
     const [data, setData] = useState([] as any[]);
     const [inputValue, setInputValue] = useState('');
+    const [usersEmpty, setUsersEmpty] = useState(false);
+    const [durationEmpty, setDurationEmpty] = useState(false);
+    const [serviceEmpty, setServiceEmpty] = useState(false);
 
     const startTest = (users: string, duration: string) => {
         setShowSpinner(true);
-
         const selectedOption = options.find(option => option.label === inputValue);
         const path = selectedOption ? selectedOption.path : '';
+
+        if (users === "" || duration === "" || selectedOption === undefined) {
+            if (users === "") {
+                setUsersEmpty(true);
+            } else {
+                setUsersEmpty(false);
+            }
+            
+            if (duration === "") {
+                setDurationEmpty(true);
+            } else {
+                setDurationEmpty(false);
+            }
+
+            if (selectedOption === undefined) {
+                setServiceEmpty(true);
+            } else {
+                setServiceEmpty(false);
+            }
+
+            setShowSpinner(false);
+            return;
+        } else {
+            setUsersEmpty(false);
+            setDurationEmpty(false);
+        }
 
         axios.get(`${process.env.API_URL}/${path}`, {
             params: {
@@ -55,16 +84,18 @@ function Scenario({ title, text }: { title: string, text: string}) {
                 <div className="content">
                     <p>{text}</p>
                     <form>
-                        <div className="mb-3">
+                        <div className="mb-3 d-flex flex-column align-items-center">
                             <p>Wie viele Nutzer sollen genutzt werden:</p>
                             <TextField id="outlined-basic" label="Anzahl Nutzer" variant="outlined" type="number" value={amountUser} onChange={(e) => setAmoutUser(e.target.value)}/>
-                        </div>
-                        <div className="mb-3">
-                            <p>Wie lange soll der Test laufen:</p>
-                            <TextField id="outlined-basic" label="Dauer" variant="outlined" type="number" value={duration} onChange={(e) => setDuration(e.target.value)}/>
+                            {usersEmpty && <Alert severity="error" className="mt-2">Bitte Anzahl der Nutzer angeben.</Alert>}
                         </div>
                         <div className="mb-3 d-flex flex-column align-items-center">
                             <p>Wie lange soll der Test laufen:</p>
+                            <TextField id="outlined-basic" label="Dauer" variant="outlined" type="number" value={duration} onChange={(e) => setDuration(e.target.value)}/>
+                            {durationEmpty && <Alert severity="error" className="mt-2">Bitte eine Dauer angeben.</Alert>}
+                        </div>
+                        <div className="mb-3 d-flex flex-column align-items-center">
+                            <p>An welchen Service soll der Test gehen:</p>
                             <Autocomplete
                                 disablePortal
                                 id="combo-box-service"
@@ -74,8 +105,9 @@ function Scenario({ title, text }: { title: string, text: string}) {
                                 setInputValue(newInputValue);
                                 }}
                                 sx={{ width: 300 }}
-                                renderInput={(params) => <TextField {...params} label="Service" />}
+                                renderInput={(params) => <TextField {...params} label="Bitte Service auswählen..." />}
                                 />
+                            {serviceEmpty && <Alert severity="error" className="mt-2">Bitte einen Service auswählen.</Alert>}
                         </div>
                         <div>
                             <Button variant="outlined" className='mb-3' onClick={() => {startTest(amountUser, duration)}}>Test starten</Button>
