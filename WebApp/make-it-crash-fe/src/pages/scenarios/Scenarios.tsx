de02d9af7e5cc7bf61da4af9801fb484750f31cc
krisschaaf <kris-schaaf@gmx.de>
2024-06-03 14:40:20 +0200

    add infos and warnings to webapp fe

--- WebApp/make-it-crash-fe/src/pages/scenarios/Scenarios.tsx
+++ WebApp/make-it-crash-fe/src/pages/scenarios/Scenarios.tsx
@@ -9,7 +9,7 @@ function Scenarios() {
                     <li className="mb-3"><Scenario title="Szenario 1" text="I'm a scenario!" path="foo"></Scenario></li>
                     <li className="mb-3"><Scenario title="Szenario 2" text="Me too!" path="bar"></Scenario></li>
                     <li className="mb-3"><Scenario title="Szenario 3" text="I'm another scenario!" path="baz"></Scenario></li>
-                    <li className="mb-3"><ScenarioWithParams title="Szenario mit Parametern" text="I'm a scenario with params!"></ScenarioWithParams></li>
+                    <li className="mb-3"><ScenarioWithParams title="Szenario mit flexiblen Parametern" text="Wähle in diesem Szenario deine bevorzugten Parameter aus."></ScenarioWithParams></li>
                 </ul>
             </div>
         )
