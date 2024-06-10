0eec140d19aab41ee519d8d36591831bc281a85a
infwjj624 <yamam.alshoufani@haw-hamburg.de>
2024-06-10 11:19:37 +0200

    fixed Visit data in DB, fixed Exception handling

--- spring-petclinic-visits-service/src/main/resources/db/hsqldb/data.sql
+++ spring-petclinic-visits-service/src/main/resources/db/hsqldb/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO visits VALUES (1,1, 7, '2013-01-01', 'rabies shot');
-INSERT INTO visits VALUES (2,1, 8, '2013-01-02', 'rabies shot');
-INSERT INTO visits VALUES (3,1, 8, '2013-01-03', 'neutered');
-INSERT INTO visits VALUES (4,1, 7, '2013-01-04', 'spayed');
+INSERT INTO visits VALUES (1, 1, 1, '2013-01-01', 'rabies shot');
+INSERT INTO visits VALUES (2, 2, 1, '2013-01-02', 'rabies shot');
+INSERT INTO visits VALUES (3, 3, 1, '2013-01-03', 'neutered');
+INSERT INTO visits VALUES (4, 4, 1, '2013-01-04', 'spayed');
