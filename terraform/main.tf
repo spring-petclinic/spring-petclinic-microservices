ae1315d4b24b7ec4e3cd0420d15f64cbc64212b8
David Molta Hernández <83254590+DavidMolta18@users.noreply.github.com>
2024-11-14 21:41:01 +0000

    pipe

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-# Grupo de recursos hoa hola hola
+# Grupo de recursos hoa hola hola dsasdasdas
 resource "azurerm_resource_group" "example_rg" {
   name     = var.resource_group_name
   location = var.location
