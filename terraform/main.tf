e6498c7094b6dd54bcb83de4aa9f980f9a09d1e5
David Molta Hernández <83254590+DavidMolta18@users.noreply.github.com>
2024-11-14 21:30:13 +0000

    Pipeline 3

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-# Grupo de recursos probando a hiola
+# Grupo de recursos hoa hola hola
 resource "azurerm_resource_group" "example_rg" {
   name     = var.resource_group_name
   location = var.location
