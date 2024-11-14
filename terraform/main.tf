7a5abe6bdbfb46c44f8ce3229e6b7a48fe4c9502
David Molta Hernández <83254590+DavidMolta18@users.noreply.github.com>
2024-11-14 22:04:30 +0000

    pipoveda

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-# Grupo de recursos david
+# Grupo de recursos david molta
 resource "azurerm_resource_group" "example_rg" {
   name     = var.resource_group_name
   location = var.location
