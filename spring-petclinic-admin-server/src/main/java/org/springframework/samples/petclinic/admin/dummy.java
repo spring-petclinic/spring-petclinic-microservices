a94ef0d8c2634d3086f5aa23afe037913fcf2755
Vind1Viet <vindiviet@gmail.com>
2025-03-09 15:23:22 +0700

    Add dummy Java file to trigger CI/CD pipeline

---
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/dummy.java
@@ -0,0 +1,6 @@
+// This is a dummy file to trigger CI/CD pipeline
+public class Dummy {
+    public static void main(String[] args) {
+        System.out.println("This is a dummy Java file.");
+    }
+}
