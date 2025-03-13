05f26e8cadd98e37b69bc59d168f0d7ca9f3f481
howtobefun <duytdn2806@gmail.com>
2025-03-13 19:55:35 +0700

    Test: Remove a spare comment

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -24,7 +24,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableAdminServer
 @EnableDiscoveryClient
 public class SpringBootAdminApplication {
-    public static void main(String[] args) { // Test comment
+    public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
 
