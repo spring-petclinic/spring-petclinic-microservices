44801f0cf3ba972526b1a4349612e9654cf0c419
Hồ Phước Nghĩa <118056302+nghiaz160904@users.noreply.github.com>
2025-03-13 11:14:57 +0700

    Update VetRepository.java

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/VetRepository.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/VetRepository.java
@@ -28,6 +28,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Maciej Szarlinski
  */
 public interface VetRepository extends JpaRepository<Vet, Integer> {
-    
+    //hello
 }
 
