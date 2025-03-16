afce347b9a2e03ea39f99a54edf756edd1668e82
thienbanho <thienbanho@gmail.com>
2025-03-16 20:12:50 +0700

    commit 2

---
+++ spring-petclinic-admin-server/src/test/java/org/springframework/samples/petclinic/admin/AdminServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class AdminServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+} 
---
+++ spring-petclinic-genai-service/src/test/java/org/springframework/samples/petclinic/vets/GenaiServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package org.springframework.samples.petclinic.vets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class GenaiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+} 
