e174c95ba2fd3673fcdecec6e3f46b0bde9162ef
Nguyễn Trần Đức Thiện <125042711+thienbanho@users.noreply.github.com>
2025-03-16 18:37:45 +0700

    Update PetResourceTest.java

--- spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
+++ spring-petclinic-customers-service/src/test/java/org/springframework/samples/petclinic/customers/web/PetResourceTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
- * @author Maciej Szarlinski
+ * @author Maciej Szarlinski add comment
  */
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(PetResource.class)
