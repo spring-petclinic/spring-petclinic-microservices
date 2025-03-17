c8d63e4b4b178c98378dde8a3a58d1d9f36f2c03
Akerman0509 <strayslugs0509@gmail.com>
2025-03-17 15:29:51 +0700

    change customer 1 in main branch check new pipeline

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -31,8 +31,3 @@ public class CustomersServiceApplication {
 	}
 }
 // adding change in customer service 1
-// adding change in customer service 2
-// adding change in customer service 3
-// adding change in customer service 4
-// adding change in customer service 5
-// adding change in customer service 6
