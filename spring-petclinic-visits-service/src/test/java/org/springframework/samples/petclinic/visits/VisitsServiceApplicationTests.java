a8b54582600d5da651f0579a4127840b35b41636
Quang Nguyen Huynh Minh <124662156+quang21122@users.noreply.github.com>
2025-03-19 17:18:25 +0700

    Update visits

---
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/VisitsServiceApplicationTests.java
@@ -0,0 +1,36 @@
+package org.springframework.samples.petclinic.visits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {
+    "spring.cloud.discovery.enabled=false",
+    "spring.cloud.config.enabled=false",
+    "spring.cloud.config.discovery.enabled=false",
+    "spring.cloud.loadbalancer.enabled=false",
+    "eureka.client.enabled=false",
+    "spring.zipkin.enabled=false"
+})
+class VisitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test that the application context loads successfully
+    }
+
+    @Test
+    void mainMethodStartsApplication() {
+        // This test verifies that the main method can be called without throwing exceptions
+        VisitsServiceApplication.main(new String[]{
+            "--spring.cloud.discovery.enabled=false",
+            "--spring.cloud.config.enabled=false",
+            "--spring.cloud.config.discovery.enabled=false",
+            "--spring.cloud.loadbalancer.enabled=false",
+            "--eureka.client.enabled=false",
+            "--spring.zipkin.enabled=false",
+            "--server.port=0"
+        });
+    }
+} 
