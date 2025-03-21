74af8ca73074f33041c465e6517972ad67d16f15
sea-temmy <selma.boulkamh.1@ens.etsmtl.ca>
2025-03-21 13:46:46 +0100

    Ajout d'un endpoint /welcome

---
+++ src/main/java/com/example/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Bienvenue dans notre application !";
+    }
+} 
