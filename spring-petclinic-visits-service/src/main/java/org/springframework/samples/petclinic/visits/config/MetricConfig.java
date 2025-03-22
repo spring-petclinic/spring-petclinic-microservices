0ca52038b82fbfbeb44ccbb04cc15aaa56e1b5d9
Pham Dao Anh Khoa <139786046+kiin21@users.noreply.github.com>
2025-03-23 00:18:42 +0700

    Update MetricConfig.java

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/config/MetricConfig.java
@@ -5,7 +5,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+// add some word
 @Configuration
 public class MetricConfig {
 
