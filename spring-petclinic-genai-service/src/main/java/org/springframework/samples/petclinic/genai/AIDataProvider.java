2e1a8ea3321376e3bac5413ddd52f3178d504103
anh khoa <pdanhkhoa21@gmail.com>
2025-03-22 13:53:26 +0700

    change scope

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/AIDataProvider.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/AIDataProvider.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class AIDataProvider {
 
-	private final VectorStore vectorStore;
+	public final VectorStore vectorStore;
     private final String ownersHostname = "http://customers-service/";
 
     private final WebClient webClient;
