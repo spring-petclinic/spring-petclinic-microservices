e5be52221b39a92a5f18b994b289e448b73f6767
Cao Tien Minh <caotienminhktvn3@gmail.com>
2025-03-24 21:51:47 +0700

    config(Jenkinsfile): test PR success or not

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import static java.util.Arrays.asList;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -31,28 +32,30 @@ class VisitResourceTest {
     @Test
     void shouldFetchVisits() throws Exception {
         given(visitRepository.findByPetIdIn(asList(111, 222)))
-                .willReturn(
-                        asList(
-                                Visit.VisitBuilder.aVisit()
-                                        .id(1)
-                                        .petId(111)
-                                        .build(),
-                                Visit.VisitBuilder.aVisit()
-                                        .id(2)
-                                        .petId(222)
-                                        .build(),
-                                Visit.VisitBuilder.aVisit()
-                                        .id(3)
-                                        .petId(222)
-                                        .build()));
+            .willReturn(
+                asList(
+                    Visit.VisitBuilder.aVisit()
+                        .id(1)
+                        .petId(111)
+                        .build(),
+                    Visit.VisitBuilder.aVisit()
+                        .id(2)
+                        .petId(222)
+                        .build(),
+                    Visit.VisitBuilder.aVisit()
+                        .id(3)
+                        .petId(222)
+                        .build()
+                )
+            );
 
         mvc.perform(get("/pets/visits?petId=111,222"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.items[0].id").value(1))
-                .andExpect(jsonPath("$.items[1].id").value(2))
-                .andExpect(jsonPath("$.items[2].id").value(3))
-                .andExpect(jsonPath("$.items[0].petId").value(111))
-                .andExpect(jsonPath("$.items[1].petId").value(222))
-                .andExpect(jsonPath("$.items[2].petId").value(222));
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.items[0].id").value(1))
+            .andExpect(jsonPath("$.items[1].id").value(2))
+            .andExpect(jsonPath("$.items[2].id").value(3))
+            .andExpect(jsonPath("$.items[0].petId").value(111))
+            .andExpect(jsonPath("$.items[1].petId").value(222))
+            .andExpect(jsonPath("$.items[2].petId").value(222));
     }
 }
