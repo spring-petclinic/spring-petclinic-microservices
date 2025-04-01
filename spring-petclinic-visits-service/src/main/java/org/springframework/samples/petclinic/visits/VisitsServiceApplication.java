a1ce7a12074fb1774a824fc70715915251e2c291
Huynh Tan Loc <12a1huynhtanloc@gmail.com>
2025-04-01 22:54:50 +0700

    Check

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -26,6 +26,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class VisitsServiceApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
