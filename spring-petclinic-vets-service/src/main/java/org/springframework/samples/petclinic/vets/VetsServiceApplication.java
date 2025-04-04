78231632f731baf0ff9eed2fbfa8682586ac628c
Pham Dao Anh Khoa <139786046+kiin21@users.noreply.github.com>
2025-04-04 15:47:04 +0700

    Update VetsServiceApplication.java

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -28,7 +28,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 @SpringBootApplication
 @EnableConfigurationProperties(VetsProperties.class)
 public class VetsServiceApplication {
-
+    // I was there
 	public static void main(String[] args) {
 		SpringApplication.run(VetsServiceApplication.class, args);
 	}
