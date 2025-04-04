756620a6447038aee55d810a5d83440cd8d886b3
ltdthao <thao452004@gmail.com>
2025-04-04 09:47:23 +0700

    modify specialty.java

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/Specialty.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/Specialty.java
@@ -41,7 +41,7 @@ public class Specialty {
     public String getName() {
         return this.name;
     }
-    // test pr check 2
+    
     public void setName(String name) {
         this.name = name;
     }
