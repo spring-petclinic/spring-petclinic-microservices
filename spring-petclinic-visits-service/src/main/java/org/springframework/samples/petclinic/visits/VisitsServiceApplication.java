60fbb92ac46c6757cdb01c50e5d95eb8f4c05d68
anh khoa <pdanhkhoa21@gmail.com>
2025-04-04 16:20:18 +0700

    feat: change services

--- spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
+++ spring-petclinic-visits-service/src/main/java/org/springframework/samples/petclinic/visits/VisitsServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class VisitsServiceApplication {
-    // I was there
+    // I was there 1
     public static void main(String[] args) {
         SpringApplication.run(VisitsServiceApplication.class, args);
     }
