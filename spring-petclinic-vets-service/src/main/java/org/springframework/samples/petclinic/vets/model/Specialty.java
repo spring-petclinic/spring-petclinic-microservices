417e0c5c712b791691c631c56cef462a0597ed6f
ltdthao <thao452004@gmail.com>
2025-04-04 09:27:00 +0700

    test pr

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/Specialty.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/model/Specialty.java
@@ -41,7 +41,7 @@ public class Specialty {
     public String getName() {
         return this.name;
     }
-
+    // test pr check
     public void setName(String name) {
         this.name = name;
     }
