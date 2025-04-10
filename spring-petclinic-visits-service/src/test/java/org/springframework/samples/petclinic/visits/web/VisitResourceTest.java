b607edcad98d78901cfc4974a6afe3730a0e5dc8
Nguyen Thi Huyen <22120143@student.hcmus.edu.vn>
2025-04-10 23:15:00 +0700

    Add test: get visits by petIds

--- spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
+++ spring-petclinic-visits-service/src/test/java/org/springframework/samples/petclinic/visits/web/VisitResourceTest.java
@@ -1,5 +1,8 @@
 package org.springframework.samples.petclinic.visits.web;
 
+import java.util.Date;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +15,14 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 
+import org.springframework.http.MediaType;
 import static java.util.Arrays.asList;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(VisitResource.class)
@@ -31,31 +37,42 @@ class VisitResourceTest {
 
     @Test
     void shouldFetchVisits() throws Exception {
+        Date date = new Date();
+
         given(visitRepository.findByPetIdIn(asList(111, 222)))
             .willReturn(
                 asList(
                     Visit.VisitBuilder.aVisit()
                         .id(1)
                         .petId(111)
+                        .date(date)
+                        .description("Visit 1")
                         .build(),
                     Visit.VisitBuilder.aVisit()
                         .id(2)
                         .petId(222)
+                        .date(date)
+                        .description("Visit 2")
                         .build(),
                     Visit.VisitBuilder.aVisit()
                         .id(3)
                         .petId(222)
+                        .date(date)
+                        .description("Visit 3")
                         .build()
                 )
             );
 
-        mvc.perform(get("/pets/visits?petId=111,222"))
+        mvc.perform(get("/pets/visits?petId=111,222").accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.items[0].id").value(1))
             .andExpect(jsonPath("$.items[1].id").value(2))
             .andExpect(jsonPath("$.items[2].id").value(3))
             .andExpect(jsonPath("$.items[0].petId").value(111))
             .andExpect(jsonPath("$.items[1].petId").value(222))
-            .andExpect(jsonPath("$.items[2].petId").value(222));
+            .andExpect(jsonPath("$.items[2].petId").value(222))
+            .andExpect(jsonPath("$.items[0].description").value("Visit 1"))
+            .andExpect(jsonPath("$.items[1].description").value("Visit 2"))
+            .andExpect(jsonPath("$.items[2].description").value("Visit 3"));
     }
 }
