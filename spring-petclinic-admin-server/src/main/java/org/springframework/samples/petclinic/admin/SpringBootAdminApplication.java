2f94acabc5233ab5b62581fdfbf55de91fb114cf
22120255 <22120255@student.hcmus.edu.vn>
2025-05-05 22:34:42 +0700

    Jenkinsfile change

--- spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
+++ spring-petclinic-admin-server/src/main/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplication.java
@@ -12,6 +12,8 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * 
  */
 package org.springframework.samples.petclinic.admin;
 
@@ -27,5 +29,4 @@ public class SpringBootAdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAdminApplication.class, args);
     }
-
 }
