ea37123a2dae014289d31016fd88e7b2ad043f63
22120197 <nguyenvinhluong242004@gmail.com>
2025-05-25 16:55:52 +0700

    Test

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CustomersServiceApplication {
-    // Ahihi ahihi ahihi ahihi
+    // Ahihi ahihi ahihi ahihi test
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
