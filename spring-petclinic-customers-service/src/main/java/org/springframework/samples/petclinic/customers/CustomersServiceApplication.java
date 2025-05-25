133d57d71a776d14cba7d51950d4b3fac36c3da5
luricisme <trinhnguyenluong182306@gmail.com>
2025-05-25 18:11:25 +0700

    Test tag

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -26,7 +26,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class CustomersServiceApplication {
     // Ahihi ahihi ahihi ahihi ahihi
-    
+
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
