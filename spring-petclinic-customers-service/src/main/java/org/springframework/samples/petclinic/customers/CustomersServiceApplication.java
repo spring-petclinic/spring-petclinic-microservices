13234cfbc1ab6052f4253f295925b967863b4be9
luricisme <trinhnguyenluong182306@gmail.com>
2025-05-25 18:09:54 +0700

    Test tag release

--- spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
+++ spring-petclinic-customers-service/src/main/java/org/springframework/samples/petclinic/customers/CustomersServiceApplication.java
@@ -25,7 +25,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class CustomersServiceApplication {
-    // Ahihi ahihi ahihi ahihi
+    // Ahihi ahihi ahihi ahihi ahihi
+    
 	public static void main(String[] args) {
 		SpringApplication.run(CustomersServiceApplication.class, args);
 	}
