83790265bb92aa683c07af48514b2ae5f9d973df
luricisme <trinhnguyenluong182306@gmail.com>
2025-05-25 15:48:15 +0700

    Test new Jenkins file

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/GenAIServiceApplication.java
@@ -25,7 +25,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GenAIServiceApplication {
-    // Ahihi
+    // Ahihi ahihi
 	public static void main(String[] args) {
 		SpringApplication.run(GenAIServiceApplication.class, args);
 	}
