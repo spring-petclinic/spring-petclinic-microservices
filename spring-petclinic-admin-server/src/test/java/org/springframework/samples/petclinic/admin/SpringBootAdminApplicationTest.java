8244a58e70718e32df2f7fa394ad5e702df64d04
Gira Chawda <girachawda101@gmail.com>
2026-07-05 19:55:01 -0400

    Create ci workflow with github actions

---
+++ spring-petclinic-admin-server/src/test/java/org/springframework/samples/petclinic/admin/SpringBootAdminApplicationTest.java
@@ -0,0 +1,14 @@
+package org.springframework.samples.petclinic.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class SpringBootAdminApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
