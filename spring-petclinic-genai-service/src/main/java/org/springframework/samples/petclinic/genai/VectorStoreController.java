aefaf7fa9eb0ab911a24c346ca0e4dc5837251c7
Antoine Rey <antoine.rey@free.fr>
2026-09-23 22:25:13 +0200

    Fix GenAI vector store loading from JAR (#564)

--- spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/VectorStoreController.java
+++ spring-petclinic-genai-service/src/main/java/org/springframework/samples/petclinic/genai/VectorStoreController.java
@@ -55,8 +55,7 @@ public class VectorStoreController {
 			// In order to save on AI credits, use a pre-embedded database that was saved
 			// to
 			// disk based on the current data in the h2 data.sql file
-			File file = resource.getFile();
-			((SimpleVectorStore) this.vectorStore).load(file);
+			((SimpleVectorStore) this.vectorStore).load(resource);
 			logger.info("vector store loaded from existing vectorstore.json file in the classpath");
 			return;
 		}
